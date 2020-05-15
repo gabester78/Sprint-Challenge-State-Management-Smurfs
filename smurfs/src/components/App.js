028b78d0a7b14b3a7b412e736bc296b6ef05d5a8
Gabriel Romero <gabester78@gmail.com>
2020-05-15 12:02:33 -0500

    Api displaying data!

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,25 +1,24 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 import { SmurfContext } from "../contexts/SmurfContext";
-import Form from "./components/Form";
 
-class App extends Component {
-  render() {
-    const [smurf, setSmurf] = useState([]);
-    console.log(smurf, "<<<smurf");
-    return (
-      <SmurfContext.Provider value={{ smurf, setSmurf }}>
-        <div className="App">
-          <h1>SMURFS! 2.0 W/ Redux</h1>
-          <div>Welcome to your state management version of Smurfs!</div>
-          <div>Start inside of your `src/index.js` file!</div>
-          <div>Have fun!</div>
-        </div>
+import Form from "./Form";
+import Cards from "./Cards.js";
+
+const App = () => {
+  const [smurfs, setSmurfs] = useState([]);
+
+  return (
+    <SmurfContext.Provider value={{ smurfs, setSmurfs }}>
+      <div className="App">
+        <h1>SMURFS! 2.0 W/ Redux</h1>
+        <div>Create a smurf below!</div>
         <Form />
-      </SmurfContext.Provider>
-    );
-  }
-}
+        <Cards />
+      </div>
+    </SmurfContext.Provider>
+  );
+};
 
 export default App;
--- smurfs/src/components/Cards.js
+++ smurfs/src/components/Cards.js
@@ -1,1 +1,35 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import { SmurfContext } from "../contexts/SmurfContext";
+import axios from "axios";
+
+const Smurfs = () => {
+  const { smurfs, setSmurfs } = useContext(SmurfContext);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3333/smurfs`)
+      .then((response) => {
+        // console.log(response.data)
+        setSmurfs(...smurfs, response.data);
+      })
+      .catch((error) => {
+        console.log("Error: ", error);
+      });
+  }, []);
+
+  return (
+    <div>
+      {smurfs.length >= 1
+        ? smurfs.map((smurf) => (
+            <div key={smurf.id}>
+              <h3>{smurf.name}</h3>
+              <p>{smurf.age}</p>
+              <p>{smurf.height}</p>
+            </div>
+          ))
+        : null}
+    </div>
+  );
+};
+
+export default Smurfs;
